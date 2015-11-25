8ef9fafd75b624aab7ef369dd52a3cafc1b65c67
Bernardo Silva <benny.stuff@gmail.com>
2015-11-25 17:22:42 +0000

    Add force option to push command.

--- src/Command/PushBaseTranslationsCommand.php
+++ src/Command/PushBaseTranslationsCommand.php
@@ -6,29 +6,54 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PushBaseTranslationsCommand extends ContainerAwareCommand
 {
+    const OPTION_FORCE = 'force';
+
     protected $container;
     protected $output;
     protected $input;
 
     protected function configure()
     {
-        $this->setName('partnermarketing:translations:push_base_translations');
+        $this->setName('partnermarketing:translations:push_base_translations')
+            ->addOption(
+                self::OPTION_FORCE,
+                null,
+                InputOption::VALUE_NONE,
+                'If set, it will not pull before push.'
+            );;
     }
 
+    /**
+     * This command push translations.
+     * It pull translations before pushing new ones.
+     *
+     * Note:
+     * When `--force` option is set, it will not pull translations before push new ones.
+     *
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     * @throws \Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $adapter = $this->getContainer()->get('partnermarketing_translation.adapter');
 
-        $pullCommandName = 'partnermarketing:translations:pull_translations';
-        $app = $this->getApplication()->find($pullCommandName);
-        $input = new ArrayInput([
-            'command' => $pullCommandName
-        ]);
-        $app->run($input, $output);
+        if (!$input->getOption(self::OPTION_FORCE)) {
+            $pullCommandName = 'partnermarketing:translations:pull_translations';
+            $app = $this->getApplication()->find($pullCommandName);
+            $input = new ArrayInput([
+                'command' => $pullCommandName
+            ]);
+            $app->run($input, $output);
+        }
 
         $result = $adapter->pushBaseTranslations();
 
